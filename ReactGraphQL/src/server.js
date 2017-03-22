d3d78ed9de1fafbb09cee9c532731edc8fd66fdb
keyrrae <xuan-wang@outlook.com>
2017-03-22 16:44:18 -0700

    first schema, GraphiQL

--- ReactGraphQL/src/server.js
+++ ReactGraphQL/src/server.js
@@ -3,9 +3,11 @@
  */
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true
 }));
 
